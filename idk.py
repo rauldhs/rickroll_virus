0273aea0dd1d95358a88dcd0aa0915d1a9cdbf80
rauldhs <67503991+rauldhs@users.noreply.github.com>
2022-08-10 02:39:48 +0300

    Add files via upload

---
+++ idk.py
@@ -0,0 +1,8 @@
+import os
+
+for i in range(0,5):
+    os.system("start /min  cmd /c cd Documents/programing_shit/rickroll_virus")
+    os.system("start /min  cmd /c python main.py")
+
+
+
---
+++ main.py
@@ -0,0 +1,51 @@
+import urllib.request
+import webbrowser
+from random import randint
+import os
+import pyglet
+
+gif = urllib.request.urlretrieve(
+  'https://c.tenor.com/q0Ejci9EQhcAAAAi/rick-astley-rick-roll.gif',
+   "rickroll.gif")
+
+
+animation = pyglet.resource.animation("rickroll.gif")
+animSprite = pyglet.sprite.Sprite(animation)
+
+w = animSprite.width 
+h = animSprite.height
+r,g,b,alpha = 0.5,0.5,0.5,0.5
+
+
+pyglet.gl.glClearColor(r,g,b,alpha)
+
+print(""" ________  ___  ________  ___  __    ________  ________  ___       ___              
+|\   __  \|\  \|\   ____\|\  \|\  \ |\   __  \|\   __  \|\  \     |\  \             
+\ \  \|\  \ \  \ \  \___|\ \  \/  /|\ \  \|\  \ \  \|\  \ \  \    \ \  \            
+ \ \   _  _\ \  \ \  \    \ \   ___  \ \   _  _\ \  \\\  \ \  \    \ \  \           
+  \ \  \\  \\ \  \ \  \____\ \  \\ \  \ \  \\  \\ \  \\\  \ \  \____\ \  \____      
+   \ \__\\ _\\ \__\ \_______\ \__\\ \__\ \__\\ _\\ \_______\ \_______\ \_______\    
+    \|__|\|__|\|__|\|_______|\|__| \|__|\|__|\|__|\|_______|\|_______|\|_______|    
+                                                                                    
+                                                                                    
+                                                                                    
+                                ___      ___ ___  ________  ___  ___  ________      
+                               |\  \    /  /|\  \|\   __  \|\  \|\  \|\   ____\     
+                               \ \  \  /  / | \  \ \  \|\  \ \  \\\  \ \  \___|_    
+                                \ \  \/  / / \ \  \ \   _  _\ \  \\\  \ \_____  \   
+                                 \ \    / /   \ \  \ \  \\  \\ \  \\\  \|____|\  \  
+                                  \ \__/ /     \ \__\ \__\\ _\\ \_______\____\_\  \ 
+                                   \|__|/       \|__|\|__|\|__|\|_______|\_________\
+                                                                        \|_________|""")
+
+class WindowsDraw():  
+    for window in range(0,4):
+        window = pyglet.window.Window(width = w,height = h,resizable=True)
+        x, y = window.get_location()
+        window.set_location(x + randint(0,1000), y + randint(0,1000))
+        @window.event
+        def on_draw():  
+            animSprite.draw()
+
+pyglet.app.run()
+
